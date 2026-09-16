7f4d4af8a6d7f962782e1f83aa037b04ef503039
Lars Birger <larsbirger.bergmaal@gmail.com>
2026-09-16 14:18:47 +0200

    created folder for O1 and O2, and started on O2

---
+++ obligatory/O2/workspace/java/o2/src/main/java/com/o2/Main.java
@@ -0,0 +1,11 @@
+package com.o2;
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame o2 = new JFrame();
+        o2.setSize(500,600);
+        o2.setVisible(true);
+        // System.out.println("Hello world!");
+    }
+}
